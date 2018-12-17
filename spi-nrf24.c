4b788eb06f7c21a4a1c8f7d43206b47dd98e4b04
Tomasz Król <tomicode@gmail.com>
2018-12-17 20:12:47 +0100

    Create device class and register spi driver.

--- spi-nrf24.c
+++ spi-nrf24.c
@@ -25,17 +25,81 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 
+#include <linux/spi/spi.h>
+
+struct nrf24_radio {
+  struct spi_device *spi;
+};
+
+static const struct file_operations nrf24_fops = {
+  .owner = THIS_MODULE,
+};
+
+static struct class *nrf24_class;
+
+static int nrf24_major_num;
+
+static int nrf24_probe(struct spi_device *spi)
+{
+  printk(KERN_INFO "nrf24_probe called\n");
+  return 0;
+}
+
+static int nrf24_remove(struct spi_device *spi)
+{
+  printk(KERN_INFO "nrf24_remove called.\n");
+  return 0;
+}
+
+static const struct of_device_id nrf24_of_ids[] = {
+  { .compatible = "nordicsemi,nrf24l01p" },
+  {},
+};
+MODULE_DEVICE_TABLE(of, nrf24_of_ids);
+
+static struct spi_driver nrf24_spi_driver = {
+  .driver = {
+    .name = "nrf",
+    .of_match_table = nrf24_of_ids,
+  },
+  .probe = nrf24_probe,
+  .remove = nrf24_remove,
+};
+
 static int __init nrf24_init(void)
 {
   printk(KERN_INFO "Hello world from nrf24 driver.\n");
 
-  // Module successful registered.
-  return 0;
+  int status = register_chrdev(0, "nrf24", &nrf24_fops);
+  if (status < 0)
+    return status;
+
+  nrf24_major_num = status;
+  
+  nrf24_class = class_create(THIS_MODULE, "nrf24radio");
+  if (IS_ERR(nrf24_class)) {
+    printk(KERN_WARNING "Unable to register nRF24 radio class.\n");
+    unregister_chrdev(nrf24_major_num, "nrf24");
+    return -EINVAL;
+  }
+
+  status = spi_register_driver(&nrf24_spi_driver);
+  if (status < 0) {
+    printk(KERN_WARNING "Unable to register nRF24 SPI Driver.\n");
+    class_destroy(nrf24_class);
+    unregister_chrdev(nrf24_major_num, "nrf24");
+  }
+
+  return status;
 }
 
 static void __exit nrf24_exit(void)
 {
-  printk(KERN_INFO "Module unloaded. Bye..\n");
+  printk(KERN_INFO "Unloading module.. Bye.\n");
+
+  spi_unregister_driver(&nrf24_spi_driver);
+  class_destroy(nrf24_class);
+  unregister_chrdev(nrf24_major_num, "nrf24");
 }
 
 module_init(nrf24_init);
