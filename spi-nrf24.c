a1dfd649755b17c307c56850dca9a94e198dee3f
Tomasz Król <tomicode@gmail.com>
2018-12-18 14:17:33 +0100

    Query radio status register. Basic cdev operations.

--- spi-nrf24.c
+++ spi-nrf24.c
@@ -17,16 +17,42 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/device.h>
-
+#include <linux/kernel.h>
+#include <linux/uaccess.h>
 #include <linux/spi/spi.h>
+
+#define NRF24_MAX_BUFFER 32
+
+#define NRF24_REG_CONFIG 0x00
+#define NRF24_REG_EN_AA 0x01
+#define NRF24_REG_EN_RXADDR 0x02
+#define NRF24_REG_SETUP_AW 0x03
+#define NRF24_REG_SETUP_RETR 0x04
+#define NRF24_REG_RF_CH 0x05
+#define NRF24_REG_RF_SETUP 0x06
+#define NRF24_REG_STATUS 0x07
+#define NRF24_REG_OBSERVE_TX 0x08
+#define NRF24_REG_RPD 0x09
+#define NRF24_REG_RX_ADDR_P0 0x0A
+#define NRF24_REG_RX_ADDR_P1 0x0B
+#define NRF24_REG_RX_ADDR_P2 0x0C
+#define NRF24_REG_RX_ADDR_P3 0x0D
+#define NRF24_REG_RX_ADDR_P4 0x0E
+#define NRF24_REG_RX_ADDR_P5 0x0F
+#define NRF24_REG_TX_ADDR 0x10
+#define NRF24_REG_RX_PW_P0 0x11
+#define NRF24_REG_FIFO_STATUS 0x17
 
 struct nrf24_radio {
     dev_t devt;
+    uint8_t in_use;
     struct spi_device *spi;
 
     struct gpio_desc *irq_gpiod;
     struct gpio_desc *ce_gpiod;
     struct gpio_desc *led_gpiod;
+
+    uint8_t *mem_buf;
 };
 
 static struct class *nrf24_class;
@@ -50,13 +76,36 @@ static long nrf24_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
     return 0;
 }
 
-static int nrf24_open(struct inode *inode, struct file *flip)
+static int nrf24_open(struct inode *inode, struct file *file)
 {
+    struct nrf24_radio *rdev = file->private_data;
+
+    if (rdev->in_use)
+        return -EBUSY;
+
+    rdev->in_use++;
+    rdev->mem_buf = kmalloc(sizeof(uint8_t) * NRF24_MAX_BUFFER, GFP_KERNEL);
+    if (!rdev->mem_buf) {
+        rdev->in_use--;
+        return -ENOMEM;
+    }
+
     return 0;
 }
 
-static int nrf24_release(struct inode *inode, struct file *flip)
+static int nrf24_release(struct inode *inode, struct file *file)
 {
+    struct nrf24_radio *rdev = file->private_data;
+
+    if (rdev->in_use == 0) {
+       printk(KERN_WARNING "Release of an invalid nrf24_radio handler.");
+       return -EINVAL;
+    }
+
+    kfree(rdev->mem_buf);
+    rdev->mem_buf = NULL;
+    rdev->in_use--;
+
     return 0;
 }
 
@@ -72,6 +121,7 @@ static int nrf24_probe(struct spi_device *spi)
 {
     struct nrf24_radio *nrf24dev;
     struct device *dev;
+    size_t status;
 
     nrf24dev = kmalloc(sizeof(*nrf24dev), GFP_KERNEL);
     if (!nrf24dev)
@@ -82,7 +132,7 @@ static int nrf24_probe(struct spi_device *spi)
 
     /* GPIOs */
     nrf24dev->led_gpiod = gpiod_get(&spi->dev, "nrf24,led", GPIOD_OUT_LOW);
-  
+
     gpiod_set_value(nrf24dev->led_gpiod, 1);
 
     dev = device_create(nrf24_class, &spi->dev, nrf24dev->devt, 
@@ -94,6 +144,15 @@ static int nrf24_probe(struct spi_device *spi)
     }
 
     spi_set_drvdata(spi, nrf24dev);
+
+    status = spi_w8r8(spi, NRF24_REG_STATUS);
+    if (status < 0) {
+        printk(KERN_WARNING "Unable to query radio status.\n");
+    }
+    else {
+        printk(KERN_INFO "Radio current status: 0x%02x\n", status);
+    }
+
     return 0;
 }
 
@@ -114,6 +173,11 @@ static int nrf24_remove(struct spi_device *spi)
     return 0;
 }
 
+static void nrf24_shutdown(struct spi_device *spi)
+{
+
+}
+
 static const struct of_device_id nrf24_of_ids[] = {
     { .compatible = "nordicsemi,nrf24l01p" },
     {},
@@ -127,6 +191,7 @@ static struct spi_driver nrf24_spi_driver = {
     },
     .probe = nrf24_probe,
     .remove = nrf24_remove,
+    .shutdown = nrf24_shutdown,
 };
 
 static int __init nrf24_init(void)
